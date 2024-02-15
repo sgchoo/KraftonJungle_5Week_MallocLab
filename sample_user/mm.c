b72580333078b6168087ee3e3029f376585aa2a3
Ubuntu <ubuntu@ip-172-31-41-216.ap-northeast-2.compute.internal>
2024-02-15 15:39:44 +0000

    Macro define and Init Function Implement

--- sample_user/mm.c
+++ sample_user/mm.c
@@ -42,12 +42,56 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+// Define basic size constants
+#define WSIZE               4
+#define DSIZE               8
+#define CHUNKSIZE           (1 << 12)
+
+#define MAX(x, y)           ((x) > (y)) ? (x) : (y)
+
+#define PACK(size, alloc)   ((size) | (alloc))
+
+#define GET(p)              (*(unsigned int *)(p))
+#define PUT(p, size)        (*(unsigned int *)(p) = size)
+
+#define GET_SIZE(p)         (GET(p) & ~0x7)
+#define GET_ALLOC(p)        (GET(p) & 0x1)
+
+#define HDRP(bp)            ((char *)(bp) - WSIZE)
+#define FTRP(bp)            ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+#define NEXT_BLKP(bp)       ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp)       ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
+static char *heap_listp;
+
 /*
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    if((heap_listp = mem_sbrk(4*WSIZE) == (void *)-1))
+        return -1;
+
+    PUT(heap_listp, 0);
+    PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));          // Prologue Header
+    PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));          // Prologue Footer
+    PUT(heap_listp + (3 * WSIZE), PACK(0, 1));              // Epilogue Header -> 왜 size == 0?
+    heap_listp += 2 * WSIZE;                                // Prologue block 다음 블록 위치 지정
+
+    if(extend_heap(CHUNKSIZE/WSIZE) == NULL)                // 왜 ChunkSize / 4byte만큼만 확장?
+        return -1;
     return 0;
+}
+
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+
+    size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+    if((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
 }
 
 /*
